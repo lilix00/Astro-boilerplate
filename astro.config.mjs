a217b4f4c4bdff6a5e908fc4fdbfe0528a92cd00
lilix00 <13428183696@163.com>
2025-03-10 09:14:49 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import robotsTxt from 'astro-robots-txt'
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
+  output: 'hybrid', // 启用混合模式（SSG + SSR）
+  adapter: netlify(),
   site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
   trailingSlash: 'ignore', // 在 Astro 配置中强制去除尾部斜杠
   markdown: {
